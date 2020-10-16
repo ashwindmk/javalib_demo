9e2524413baba74c9110870f5c0d4a0232b1713d
Ashwin <ashwin.dinesh01@gmail.com>
2020-10-16 13:01:00 +0530

    First commit

---
+++ src/main/java/com/ashwin/java/JavaLibDemo.java
@@ -0,0 +1,10 @@
+package com.ashwin.java;
+
+import com.github.ashwindmk.JavaLib;
+
+public class JavaLibDemo {
+    public static void main(String[] args) {
+        JavaLib.log("Hello JavaLib");
+        System.out.println("Sum: " + JavaLib.add(12, 3));
+    }
+}
